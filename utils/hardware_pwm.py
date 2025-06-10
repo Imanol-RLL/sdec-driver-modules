958ff7be88815ddc84524995b26a9a69b762c396
Facundo Galvagno <galvagno.facundo@outlook.com>
2025-06-10 02:46:18 -0300

    update freqs

--- utils/hardware_pwm.py
+++ utils/hardware_pwm.py
@@ -4,18 +4,18 @@ import time
 pi = pigpio.pi()
 # Script to generate a square wave using hardware PWM on Raspberry Pi
 
-# Use GPIO26
+# Use GPIO12
 gpio_pin_a = 12
-frequency = 0.5  # 0.5Hz square wave
+frequency = 1  # 1Hz square wave
 duty_cycle = 500000  # 50% duty (range: 0 to 1_000_000)
 
 pi.hardware_PWM(gpio_pin_a, frequency, duty_cycle)
 print("Generating square wave on GPIO pin {}".format(gpio_pin_a))
 
-# Use GPIO19
+# Use GPIO13
 gpio_pin_b = 13
-frequency = 0.2  # Hz square wave
-duty_cycle = 500000  # 50% duty (range: 0 to 1_000_000)
+frequency = 1  # Hz square wave
+duty_cycle = 750000  # 75% duty (range: 0 to 1_000_000)
 pi.hardware_PWM(gpio_pin_b, frequency, duty_cycle)
 print("Generating square wave on GPIO pin {}".format(gpio_pin_b))
 
