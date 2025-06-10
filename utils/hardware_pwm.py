2b8f6a5542c534b66abf40bbb5635b1da42154f1
Facundo Galvagno <galvagno.facundo@outlook.com>
2025-06-10 02:06:26 -0300

    añadido: output de señal en los dos pines

--- utils/hardware_pwm.py
+++ utils/hardware_pwm.py
@@ -3,15 +3,22 @@ import time
 
 pi = pigpio.pi()
 # Script to generate a square wave using hardware PWM on Raspberry Pi
+
 # Use GPIO26
-gpio_pin = 26
-frequency = 1000  # 1 kHz square wave
+gpio_pin_a = 26
+frequency = 0.5  # 0.5Hz square wave
 duty_cycle = 500000  # 50% duty (range: 0 to 1_000_000)
 
-pi.hardware_PWM(gpio_pin, frequency, duty_cycle)
+pi.hardware_PWM(gpio_pin_a, frequency, duty_cycle)
+print("Generating square wave on GPIO pin {}".format(gpio_pin_a))
 
-
-print("Generating square wave on GPIO pin {}".format(gpio_pin))
+# Use GPIO19
+gpio_pin_b = 26
+frequency = 0.2  # Hz square wave
+duty_cycle = 500000  # 50% duty (range: 0 to 1_000_000)
+pi.hardware_PWM(gpio_pin_b, frequency, duty_cycle)
+print("Generating square wave on GPIO pin {}".format(gpio_pin_b))
 
 time.sleep(10)  # Output square wave for 10 seconds
-pi.hardware_PWM(gpio_pin, 0, 0)  # Stop PWM
+pi.hardware_PWM(gpio_pin_a, 0, 0)  # Stop PWM
+pi.hardware_PWM(gpio_pin_b, 0, 0)  # Stop PWM
